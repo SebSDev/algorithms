c10958bd7a0bbd926f42f3b9fcfb4f9a514065af
SebSDev <sebastian.schaeffler8@gmail.com>
2018-11-10 18:13:16 +0100

    - added some comments

---
+++ src/algorithms/Searching.java
@@ -0,0 +1,48 @@
+package algorithms;
+
+/**
+ * @author Sebastian Schäffler
+ * created at 06.11.2018
+ * description: Implementing some searching algorithms in this class
+ */
+public class Searching
+{
+    /**
+     * searches an array for the i-th biggest element
+     * @param a array to search in
+     * @param l left border index
+     * @param r right border index
+     * @param i i-th biggest element is searched in the array
+     * @return the i-th biggest element (the actual value, not the index)
+     */
+    public static int quickSelect (int[] a, int l, int r, int i)
+    {
+        // l and r index are on the same element. We found our element
+        if (l == r)
+        {
+            return a[l];
+        }
+
+        // pivot
+        int p = AlgorithmUtils.partition(a, l, r);
+
+        // relative index of the pivot element
+        int k = p - l - 1;
+
+        // if the relative index == i we got the element
+        if (k == i)
+        {
+            return a[p];
+        }
+        // the searched element is in the left partition
+        else if (i < k)
+        {
+            return quickSelect(a, l, p - 1, i);
+        }
+        // the searched element is in the right partition
+        else
+        {
+            return quickSelect(a, p + 1, r,  i - k);
+        }
+    }
+}
